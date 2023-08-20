a62a8f35aa335cdd4d07c08aa885bad0a316547d
ddrous[macos] <rd.nzoyemngueguin@bristol.ac.uk>
2023-08-21 00:43:26 +0100

    Defining the structure of the app

---
+++ README.md
@@ -0,0 +1,22 @@
+# IziEdu's Search Engine
+
+A new Flutter project for the search engine app of IziEdu.
+
+## Getting Started
+
+This project is a starting point for a Flutter application. To run this project, you need to follow these steps:
+1) Install flutter sdk: https://flutter.dev/docs/get-started/install
+2) Make sure Chrome is installed as well (or if you prefer Edge, follow [this tip](https://stackoverflow.com/questions/66631883/cannot-find-chrome-try-setting-chrome-executable-to-a-chrome-executable-flutte/69859180#69859180))
+3) Navigate to this project's directory: `iziedu/searchengine/`
+4) Run the app with `flutter run -d chrome`
+
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+- [The flutter starter app with high-level explanations](./lib/legacymain.dart)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NodeLearn',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  List<List<String>> datasets = [];
+  List<List<String>> losses = [];
+  List<List<String>> accuracies = [];
+  List<List<String>> predictions = [];
+  List<List<String>> crowdwork = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    datasets.add(["MNIST", ""]);
+    datasets.add(["Fasion MNIST", "selected"]);
+    datasets.add(["SVHN", ""]);
+    datasets.add(["CIFAR-10", ""]);
+    datasets.add(["Cats vs Dogs", ""]);
+    datasets.add(["Moons", ""]);
+    datasets.add(["Celeb-A", ""]);
+    datasets.add(["Youtube-8M", ""]);
+    datasets.add(["Jeopardy", ""]);
+
+    losses.add(["MSE", "selected"]);
+    losses.add(["MAE", ""]);
+    losses.add(["KL Div", ""]);
+
+    accuracies.add(["Acc", "selected"]);
+    accuracies.add(["Precision", ""]);
+    accuracies.add(["Recall", ""]);
+    accuracies.add(["Visual Insp.", ""]);
+
+    predictions.add(["Real vs. Pred", ""]);
+    predictions.add(["Draw something", "selected"]);
+
+    crowdwork.add(["Upload labelled data !", ""]);
+    crowdwork.add(["Create synthetic data", "selected"]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenwidth = MediaQuery.of(context).size.width;
+    double screenheight = MediaQuery.of(context).size.height;
+    var padding = MediaQuery.of(context).padding;
+    double safeheight = screenheight - padding.top - padding.bottom;
+
+    double listheight = safeheight / 6;
+    double listwidth = screenwidth / 2;
+
+    void _trainModel() {}
+    void _predictFromModel() {}
+    void _saveExample() {}
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurpleAccent.shade200,
+          title: Text("NodeLearn"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: listheight,
+                width: listwidth,
+                child: ScrollableList(
+                  items: datasets,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.red,
+                ),
+              ),
+              SizedBox(
+                height: listheight,
+                width: listwidth,
+                child: ScrollableList(
+                  items: losses,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.lightGreen,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: _trainModel,
+                tooltip: 'Train the model',
+                child: const Text("Train"),
+              ),
+              SizedBox(
+                height: listheight,
+                width: listwidth,
+                child: ScrollableList(
+                  items: predictions,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.lightBlue,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: _predictFromModel,
+                tooltip: 'Predict from the trained model',
+                child: const Text("Predict"),
+              ),
+              SizedBox(
+                height: listheight,
+                width: listwidth,
+                child: ScrollableList(
+                  items: crowdwork,
+                  backgroundColor: Colors.white,
+                  foregroundColor: Colors.deepPurpleAccent.shade200,
+                ),
+              ),
+              FloatingActionButton(
+                onPressed: _saveExample,
+                tooltip: 'Save the example to the dataset',
+                child: const Text("Save"),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class ScrollableList extends StatefulWidget {
+  final List<List<String>> items;
+  final Color backgroundColor, foregroundColor;
+
+  const ScrollableList(
+      {super.key,
+      required this.items,
+      this.backgroundColor = Colors.white,
+      this.foregroundColor = Colors.lightGreen});
+
+  @override
+  State<StatefulWidget> createState() => ScrollableListState();
+}
+
+class ScrollableListState extends State<ScrollableList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.items.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            child: Container(
+              height: 80,
+              color: widget.items[index][1] == "selected"
+                  ? widget.foregroundColor
+                  : widget.backgroundColor,
+              child: Center(child: Text(widget.items[index][0])),
+            ),
+            onTap: () {
+              for (final item in widget.items) {
+                if (item[1] == 'selected') item[1] = '';
+              }
+              setState(() => widget.items[index][1] = 'selected');
+            },
+          );
+        });
+  }
+}
